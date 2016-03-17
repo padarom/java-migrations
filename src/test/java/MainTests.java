63be236f5610d42b0586c089d4061c2ab1708c13
Christopher Mühl <github@padarom.io>
2016-03-17 12:35:53 +0100

    Add first basic test

---
+++ src/test/java/MainTests.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTests {
+
+    @Test
+    public void testTests() {
+        assertEquals(7, 7);
+    }
+}
